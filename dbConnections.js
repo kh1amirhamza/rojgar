2a6d9b5cabc52e82d907c4ba78a265a3ffc7bf2a
unknown <kh1amirhamza@gmail.com>
2022-09-23 01:06:19 +0600

    new Commit

--- dbConnections.js
+++ dbConnections.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = {useNewUrlParser: true, useUnifiedTopology: true };
-const catCashDbConnectUrl = "mongodb+srv://rojgar:rojgar@cluster0.p4fnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-
+//const catCashDbConnectUrl = "mongodb+srv://rojgar:rojgar@cluster0.p4fnz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const catCashDbConnectUrl = "mongodb+srv://rojgar:rojgar@cluster0.4ymswlv.mongodb.net/?retryWrites=true&w=majority"
 module.exports = {
     getCattCashDbConnection(){
         return mongoose.createConnection(catCashDbConnectUrl, config, function(error, result){
